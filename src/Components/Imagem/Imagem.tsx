591a0d90b92d342ead7d41d9d68e54b1053011e3
Aristeu-Miranda <aristeujose1999@gmail.com>
2023-08-09 20:20:55 -0300

    feat: add routes

--- src/Components/Imagem/Imagem.tsx
+++
@@ -1,9 +0,0 @@
-import { ImagemProps } from "./Imagem.types"
-
-function Imagem({ src, alt }: ImagemProps) {
-    return (
-        <img src={src} alt={alt}/>
-    )
-}
-
-export default Imagem
--- src/Components/Imagem/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default } from "./Imagem";
-export type { ImagemProps } from "./Imagem.types";
