ffe64deb340ba78088dafb4d9a1abcdad1cf7c65
Aristeu-Miranda <aristeujose1999@gmail.com>
2023-12-07 00:33:44 -0300

    refactor: layout cardnote

--- src/Components/CardNota/CardNota.tsx
+++ src/Components/CardNota/CardNota.tsx
@@ -1,25 +1,25 @@
 import Botao from "../Botao/Botao";
 import { CardNotaProps } from "./CardNota.types";
 import { AiOutlineClose } from 'react-icons/ai'
+import { FaBuilding } from "react-icons/fa";
+
 
 function CardNota({ name, contract, service, portion, status, dateMaxSend, id, removeNotas}: CardNotaProps) {
     const statusClass = status === 'Pendente' ? 'text-red-500 font-bold' : 'text-green-500 font-bold';
 
     return (
         <div>
-            <div className="sm:flex sm:text-center sm:text-base bg-slate-100 mb-1 py-2 rounded-lg border border-black">
-                <div className="lg:w-64 xl:w-72 2xl:w-80 mx-4">{name}</div>
-                <div className="lg:w-64 xl:w-72 2xl:w-80 mx-4">{service}</div>
-                <div className="lg:w-24 xl:w-32 2xl:w-44 mx-1">{contract}</div>
-                <div className="lg:w-24 xl:w-32 2xl:w-44 mx-1">{portion}</div>
-                <div className="lg:w-24 xl:w-32 2xl:w-44 mx-1">{dateMaxSend}</div>
-                <div className={`lg:w-24 xl:w-32 2xl:w-44 mx-1 ${statusClass}`}>{status}</div>
-            </div>
-            <div>
+            <div className="bg-slate-100 mb-1 py-2 rounded-lg text-center w-60 h-72 border border-black relative">
+                <div className="h-2/4 my-4"><FaBuilding className="mx-auto w-28 h-28 text-neutral-500" /></div>
+                <div className="mb-2 font-semibold">Cliente: {name}</div>
+                <div className="mb-3">Contrato n°: {contract}</div>
+                <Botao className="text-lime-600 transition hover:font-bold hover:text-lime-900 hover:underline">Acessar</Botao>
                 <Botao 
-                className={"bg-black text-white p-1 rounded-md relative -right-full lg:-right-[980px] xl:-right-[1175px] 2xl:-right-[1430px] -top-5"}
+                className={"bg-black text-white p-1 rounded-md absolute -top-3 -right-3"}
                 onClick={() => removeNotas(id)}
-                ><AiOutlineClose /></Botao></div>
+                ><AiOutlineClose /></Botao>
+            </div>
+                
         </div>
     )
 }
--- src/Pages/NewNota/index.tsx
+++ src/Pages/NewNota/index.tsx
@@ -1,7 +1,7 @@
 import CardNota from "../../Components/CardNota/CardNota";
-import { Cliente } from "./NewNota.type"
+import { Cliente } from "./NewNota.type";
 import Botao from "../../Components/Botao/Botao";
-import { AiOutlineFileAdd } from 'react-icons/ai'
+import { AiOutlineFileAdd } from "react-icons/ai";
 import { useEffect, useState } from "react";
 import ModalCadastrar from "../../Components/Modal/Modal";
 import Form from "../../Components/Form/Form";
@@ -10,97 +10,95 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function NewNota() {
-    const [modalOpen, setModalOpen] = useState(false);
-    function usingModal() {
-        if (modalOpen) {
-            setModalOpen(false)
-        } else setModalOpen(true)
-    }
-    
-    const [dados, setDados] = useState<Cliente[]>([])
-    useEffect(() => {
-        const urlApiClients = 'https://controle-api-mhpv.onrender.com/notes';
-        fetchClients(urlApiClients)
-        .then(data => {
-            setDados(data)
-        })
-    }, [])
+  const [modalOpen, setModalOpen] = useState(false);
+  function usingModal() {
+    if (modalOpen) {
+      setModalOpen(false);
+    } else setModalOpen(true);
+  }
 
-    const removeNotas = (id: any) => {
-        fetch(`https://controle-api-mhpv.onrender.com/notes/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then((resp) => resp.json())
-        .then(() => {
-            setDados(dados.filter((dado) => dado._id !== id))
-        })
-        toast.success("Registro removido!", {
-            position: "top-right",
-            autoClose: 3000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-    }
+  const [dados, setDados] = useState<Cliente[]>([]);
+  useEffect(() => {
+    const urlApiClients = "https://controle-api-mhpv.onrender.com/notes";
+    fetchClients(urlApiClients).then((data) => {
+      setDados(data);
+    });
+  }, []);
 
-    return (
-        <>
-            {dados.length > 0 ? (
-                <div className="sm:flex sm:flex-col sm:justify-center sm:items-center sm:mt-4">
-                <ul className="sm:grid sm:grid-cols-8 sm:gap-x-14 sm:p-2 sm:text-center sm:text-base bg-zinc-900 text-slate-100 mb-5 py-1 rounded-lg">
-                    <li className="sm:col-span-2">Nome</li>
-                    <li className="sm:col-span-2">Serviço</li>
-                    <li>N° do Contrato</li>
-                    <li>N° da Parcela</li>
-                    <li>Data para envio</li>
-                    <li>Status</li>
-                </ul>
-                <div className="flex flex-col justify-center items-center">
-                        {dados.map((cliente: Cliente) => (
-                            <CardNota
-                                key={cliente._id}
-                                name={cliente.name}
-                                contract={cliente.contract}
-                                service={cliente.service}
-                                portion={cliente.portion}
-                                status={cliente.status}
-                                dateMaxSend={cliente.date}
-                                removeNotas={removeNotas} 
-                                id={cliente._id}/>))}
-                </div>
-                <Botao 
-                    className="p-4 rounded-full text-white bg-green-500 text-6xl fixed bottom-24 right-36 hover:drop-shadow-2xl hover:-translate-y-3 transition hover:bg-green-700 duration-300"
-                    onClick={usingModal}
-                >
-                    <AiOutlineFileAdd />
-                </Botao>
-                <ModalCadastrar openmodal={modalOpen} closemodal={usingModal}>
-                    <Form />
-                </ModalCadastrar>
-            </div>
-            ) : (
-                <div className="w-1/2 text-center mx-auto mt-10">
-                    <ToastContainer />
-                    <p className="text-2xl font-medium mb-20 mt-20">Você não possui notas cadastradas... Comece agora!!!</p>
-                    <Botao 
-                    className="p-4 rounded-full text-white bg-green-500 text-6xl hover:drop-shadow-2xl hover:-translate-y-3 transition hover:bg-green-700 duration-300"
-                    onClick={usingModal}
-                >
-                    <AiOutlineFileAdd />
-                </Botao>
-                <ModalCadastrar openmodal={modalOpen} closemodal={usingModal}>
-                    <Form />
-                </ModalCadastrar>
-                </div>
-            )}
-        </>
-    )
+  const removeNotas = (id: any) => {
+    fetch(`https://controle-api-mhpv.onrender.com/notes/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then(() => {
+        setDados(dados.filter((dado) => dado._id !== id));
+      });
+    toast.success("Registro removido!", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+
+  return (
+    <>
+      {dados.length > 0 ? (
+        <div className="sm:flex sm:flex-col sm:justify-center sm:items-center sm:mt-4">
+          <div className="w-2/6 text-center sm:text-2xl bg-zinc-900 text-slate-100 mb-10 py-1 rounded-lg">
+            <h2>Consulte aqui suas notas a serem enviadas</h2>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            {dados.map((cliente: Cliente) => (
+              <CardNota
+                key={cliente._id}
+                name={cliente.name}
+                contract={cliente.contract}
+                service={cliente.service}
+                portion={cliente.portion}
+                status={cliente.status}
+                dateMaxSend={cliente.date}
+                removeNotas={removeNotas}
+                id={cliente._id}
+              />
+            ))}
+          </div>
+          <Botao
+            className="p-4 rounded-full text-white bg-green-500 text-6xl fixed bottom-24 right-36 hover:drop-shadow-2xl hover:-translate-y-3 transition hover:bg-green-700 duration-300"
+            onClick={usingModal}
+          >
+            <AiOutlineFileAdd />
+          </Botao>
+          <ModalCadastrar openmodal={modalOpen} closemodal={usingModal}>
+            <Form />
+          </ModalCadastrar>
+        </div>
+      ) : (
+        <div className="w-1/2 text-center mx-auto mt-10">
+          <ToastContainer />
+          <p className="text-2xl font-medium mb-20 mt-20">
+            Você não possui notas cadastradas... Comece agora!!!
+          </p>
+          <Botao
+            className="p-4 rounded-full text-white bg-green-500 text-6xl hover:drop-shadow-2xl hover:-translate-y-3 transition hover:bg-green-700 duration-300"
+            onClick={usingModal}
+          >
+            <AiOutlineFileAdd />
+          </Botao>
+          <ModalCadastrar openmodal={modalOpen} closemodal={usingModal}>
+            <Form />
+          </ModalCadastrar>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default NewNota
+export default NewNota;
