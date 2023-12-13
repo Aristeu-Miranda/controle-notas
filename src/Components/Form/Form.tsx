ddd8240a44e7ed1a77da01506892577ac4b38b26
Aristeu-Miranda <aristeujose1999@gmail.com>
2023-12-13 08:30:36 -0300

    refactor: form

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -52,59 +52,59 @@ function Form() {
                 <p className='text-base'>Insira os dados abaixo para cadastrar uma nova nota fiscal para controle!</p>
             </div>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="my-4 px-11">
-                    <label className="my-auto mr-20 text-start" htmlFor="name">Cliente</label>
+                <div className="my-4 mx-auto w-1/2 flex justify-around">
+                    <label className="my-auto w-1/3 text-start" htmlFor="name">Cliente</label>
                     <input  
-                        className='p-2 rounded-lg w-80' 
+                        className='p-2 rounded-lg w-2/3' 
                         type='text'
                         placeholder='Ex.: Cond / Empresa'
                         {...register("name")}
                     />
                     <span className="block my-1 text-red-700">{errors?.name?.message}</span>
                 </div>
-                <div className="my-4 px-11">
-                    <label className="my-auto mr-20 text-start" htmlFor="service">Serviço</label>
+                <div className="my-4 mx-auto w-1/2 flex justify-around">
+                    <label className="my-auto w-1/3 text-start" htmlFor="service">Serviço</label>
                     <input 
-                        className='p-2 rounded-lg w-80' 
+                        className='p-2 rounded-lg w-2/3' 
                         type='text'
                         placeholder='Descreva o tipo de serviço prestado'
                         {...register("service")}
                     />
                     <span className="block my-1 text-red-700">{errors?.service?.message}</span>
                 </div>
-                <div className="my-4 px-11">
-                    <label className="my-auto mr-5 text-start" htmlFor="contract">Nº do contrato</label>
+                <div className="my-4 mx-auto w-1/2 flex justify-around">
+                    <label className="my-auto w-1/3 text-start" htmlFor="contract">Nº do contrato</label>
                     <input
                         {...register("contract")}
-                        className='p-2 rounded-lg w-80' 
+                        className='p-2 rounded-lg w-2/3' 
                         type='text'
                         placeholder='Ex.: XXXX-XX/YY-X'
                     />
                     <span className="block my-1 text-red-700">{errors?.contract?.message}</span>
                 </div>
-                <div className="my-4 text-start pl-[218px]">
-                    <label className="my-auto mr-7 text-start" htmlFor="portion">Quantidade de parcelas</label>
+                <div className="my-4 mx-auto w-1/2 flex justify-around">
+                    <label className="my-auto w-1/3 text-start" htmlFor="portion">Nº de parcelas</label>
                     <input 
                         {...register("portion")} 
-                        className='p-2 rounded-lg w-80' 
+                        className='p-2 rounded-lg w-2/3' 
                         type='text'
                         placeholder='Ex.: 10'
                     />
                     <span className="block my-1 text-red-700">{errors?.portion?.message}</span>
                 </div>
-                <div className="my-4 px-11">
-                    <label className="my-auto mr-5 text-start" htmlFor="client">Dia para envio</label>
+                <div className="my-4 mx-auto w-1/2 flex justify-around">
+                    <label className="my-auto w-1/3 text-start" htmlFor="client">Dia para envio</label>
                     <input 
                         {...register("date")}
-                        className='p-2 rounded-lg w-80' 
+                        className='p-2 rounded-lg w-2/3' 
                         type='text'
                         placeholder='Ex.: 25'
                     />
                     <span className="block my-1 text-red-700">{errors?.date?.message}</span>
                 </div>
-                <div className="my-4 pl-[130px] relative">
-                    <label className="my-auto mr-12 absolute top left-80" htmlFor='status'>Descrição</label>
-                    <textarea {...register("status")} className="p-4 rounded-lg w-80 h-48">
+                <div className="my-4 mx-auto w-1/2 flex justify-around">
+                    <label className="top w-1/3 text-start" htmlFor='status'>Descrição</label>
+                    <textarea {...register("status")} className="p-4 rounded-lg w-2/3 h-48">
                     </textarea>
                 </div>
                 <Botao type="submit" className="text-white p-2 mt-50 bg-green-500 rounded-lg transition hover:bg-green-700 duration-300">Cadastrar nota fiscal</Botao>
