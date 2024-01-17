3e87224db697b0ec063be1aa2feb6abd6010642f
Aristeu-Miranda <aristeujose1999@gmail.com>
2024-01-16 23:51:24 -0300

    update infos notes

--- src/Components/DescriptionNote/DescriptionNote.tsx
+++ src/Components/DescriptionNote/DescriptionNote.tsx
@@ -4,6 +4,7 @@ import Botao from "../Botao/Botao";
 import { DescriptionNotesProps } from "./DescriptionNote.types";
 import { MdModeEdit, } from "react-icons/md";
 import { FaFilePdf } from "react-icons/fa";
+import clsx from 'clsx';
 
 function DescriptionNote({
   name,
@@ -16,11 +17,15 @@ function DescriptionNote({
 
   const [able, setAble] = useState(true)
 
+  const editInfosNote = () => {
+    setAble(false)
+  }
+
 
   const updateNote = () => {
     const urlApiClientsUpdate = "https://controle-api-mhpv.onrender.com/notes";
     fetchUpdate(urlApiClientsUpdate)
-    setAble(false)
+    
   }
 
   return (
@@ -30,42 +35,42 @@ function DescriptionNote({
               Informações sobre sua nota fiscal
           </div>
           <div className="flex justify-evenly pt-2 mx-auto w-32">
-            <Botao className="bg-gray-600 text-white text-1xl p-2 rounded-lg hover:shadow-xl hover:-translate-y-1 transition" onClick={updateNote}>
+            <Botao className="bg-gray-600 text-white text-1xl p-2 rounded-lg hover:shadow-xl hover:-translate-y-1 transition" onClick={editInfosNote}>
               <MdModeEdit />
             </Botao>
-            <Botao className="bg-gray-600 text-white text-1xl p-2 rounded-lg hover:shadow-xl hover:-translate-y-1 transition" onClick={updateNote}>
+            <Botao className="bg-gray-600 text-white text-1xl p-2 rounded-lg hover:shadow-xl hover:-translate-y-1 transition">
               <FaFilePdf />
             </Botao>
           </div>
       </div>
       <form className="text-xl bg-white w-2/3 mx-auto rounded-lg p-5 shadow-2xl overflow-y-auto">
-        <div className="my-2 p-3 border-b-2 border-black">
+        <div className="my-2 border-b-2 border-black">
           <label htmlFor="name" className="font-medium block text-lg">Cliente:</label>
-          <input type="text" value={name} className="text-center text-sm" disabled={able}/>
+          <input type="text" value={name} className={clsx('text-center text-sm w-full p-2 rounded-md', able ? 'bg-slate-200' : '')} disabled={able} />
         </div>
-        <div className="my-4 p-3 border-b-2 border-black">
+        <div className="my-4 border-b-2 border-black">
           <label htmlFor="name" className="font-medium block text-lg">Serviço executado:</label>
-          <input type="text" value={service} className="text-center text-sm w-full"/>
+          <input type="text" value={service} className={clsx('text-center text-sm w-full p-2 rounded-md', able ? 'bg-slate-200' : '')} disabled={able}/>
         </div>
-        <div className="my-4 p-3 border-b-2 border-black">
+        <div className="my-4 border-b-2 border-black">
           <label htmlFor="name" className="font-medium block text-lg">Nº do Contrato:</label>
-          <input type="text" value={contract} className="text-center text-sm"/>
+          <input type="text" value={contract} className={clsx('text-center text-sm w-full p-2 rounded-md', able ? 'bg-slate-200' : '')} disabled={able}/>
         </div>
-        <div className="my-4 p-3 border-b-2 border-black">
+        <div className="my-4 border-b-2 border-black">
           <label htmlFor="name" className="font-medium block text-lg">Quantidade de parcelas:</label>
-          <input type="text" value={portion} className="text-center text-sm"/>
+          <input type="text" value={portion} className={clsx('text-center text-sm w-full p-2 rounded-md', able ? 'bg-slate-200' : '')} disabled={able}/>
         </div>
-        <div className="my-4 p-3 border-b-2 border-black">
+        <div className="my-4 border-b-2 border-black">
           <label htmlFor="name" className="font-medium mb-3 block text-lg">Dia para envio em cada mês:</label>
-          <input type="text" value={dateMaxSend} className="text-center text-sm w-full"/>
+          <input type="text" value={dateMaxSend} className={clsx('text-center text-sm w-full p-2 rounded-md', able ? 'bg-slate-200' : '')} disabled={able}/>
         </div>
-        <div className="my-2 p-3">
+        <div className="my-2">
           <label htmlFor="name" className="font-medium mb-3 block text-lg">Descrição da Nota fiscal:</label>
-          <textarea className="text-start text-sm w-full h-24" disabled={able}>{status}</textarea>
+          <textarea className={clsx('text-start p-2 rounded-md text-sm w-full h-24', able ? 'bg-slate-200' : '')} disabled={able}>{status}</textarea>
         </div>
         <div>
-          <Botao className="">
-            Enviar
+          <Botao className={clsx(able ? 'hidden' : 'block mx-auto text-sm text-white bg-green-500 rounded-lg transition hover:bg-green-700 hover:font-medium p-2')} onClick={updateNote}>
+            Atualizar dados
           </Botao>
         </div>
       </form>   
